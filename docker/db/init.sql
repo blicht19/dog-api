179d28246064416a443bdb7704d3b2ba9c2485b8
Bailey Licht <baileymlicht@gmail.com>
2024-09-25 19:26:01 -0500

    Finalize db setup

---
+++ docker/db/init.sql
@@ -0,0 +1,29 @@
+CREATE TYPE BREED_SIZE AS ENUM ('Tiny', 'Small', 'Medium', 'Large');
+
+CREATE domain RATING AS INTEGER CHECK (VALUE BETWEEN 1 AND 5);
+
+CREATE domain UINT AS SMALLINT CHECK (VALUE >= 0);
+
+CREATE TYPE DOG_GENDER AS ENUM ('Male', 'Female', 'Unknown');
+
+CREATE TABLE
+    breed (
+        name CHARACTER VARYING(255) PRIMARY KEY,
+        size BREED_SIZE,
+        friendliness RATING,
+        trainability RATING,
+        sheddingamount RATING,
+        exerciseneeds RATING
+    );
+
+CREATE TABLE
+    dog (
+        id serial PRIMARY key,
+        name CHARACTER VARYING(255) NOT NULL,
+        age UINT,
+        breed CHARACTER VARYING(255) REFERENCES breed (name),
+        gender DOG_GENDER,
+        color CHARACTER VARYING(255),
+        favoritefood CHARACTER VARYING(255),
+        favoritetoy CHARACTER VARYING(255)
+    );
